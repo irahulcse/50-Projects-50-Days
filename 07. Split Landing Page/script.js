ce0b374a6ceeb51d6ff8a99e56eea91277bd4c5a
irahulcse <1rahulchandra1@gmail.com>
2022-08-10 09:16:35 +0200

    07-landing-page-changes

--- 07. Split Landing Page/script.js
+++ 07. Split Landing Page/script.js
@@ -1,10 +1,16 @@
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const container = document.querySelector(".container");
 
-left.addEventListener("mouseenter", () => container.classList.add("hover-left"));
-left.addEventListener("mouseleave", () => container.classList.remove("hover-left"));
 
-right.addEventListener("mouseenter", () => container.classList.add("hover-right"));
-right.addEventListener("mouseleave", () => container.classList.remove("hover-right"));
+// Learnt about the querySelector and how to add and remove parameter within the components
+// left=document.querysSelector('.left')
+//left.addEventListner('mouseLeave', ()=>container.classList.add('hover-left'))
+const left=document.querySelector('.left')
+const right=document.querySelector('.right')
+const container=document.querySelector('.container')
 
+left.addEventListener('mouseenter',()=>container.classList.add('hover-left'));
+left.addEventListener('mouseleave',()=>container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter',()=>container.classList
+.add('hover-right'))
+right.addEventListener('mouseleave',()=>container.classList
+.remove('hover-right'))
