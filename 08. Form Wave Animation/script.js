8ca6a01c22b6a5fef39cf226c27c1346af6f357c
irahulcse <1rahulchandra1@gmail.com>
2022-08-16 09:43:30 +0200

    08. Form wave component

--- 08. Form Wave Animation/script.js
+++ 08. Form Wave Animation/script.js
@@ -6,3 +6,7 @@ labels.forEach((label) => {
         .map((letter, idx) => `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`)
         .join("");
 });
+
+
+// quite complicated to understand 
+// as we are adding a new span tage after the part
