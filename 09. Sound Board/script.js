6c9927013b91e93f3d0fd0e57ea9aec5db602aec
irahulcse <1rahulchandra1@gmail.com>
2022-08-25 21:58:09 +0200

    50 Project 50 Days : 09 Sounds Board Code committed

--- 09. Sound Board/script.js
+++ 09. Sound Board/script.js
@@ -1,22 +1,22 @@
 const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
 sounds.forEach((sound) => {
-    const btn = document.createElement("button");
-    btn.classList.add("btn");
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
 
-    btn.innerText = sound;
-    btn.addEventListener("click", () => {
-        stopSongs();
-        document.getElementById(sound).play();
-    });
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
 
-    document.getElementById("buttons").appendChild(btn);
+  document.getElementById("buttons").appendChild(btn);
 });
 
 function stopSongs() {
-    sounds.forEach((sound) => {
-        song = document.getElementById(sound);
-        song.pause();
-        song.currentTime = 0; 
-    });
+  sounds.forEach((sound) => {
+    song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
 }
